9f6a15cb916d65f290347eca0afd4b9ae6c60321
Rifai Kridolaksono <rivayx@gmail.com>
2026-07-01 05:52:18 +0700

    fix: keep branch pic modal above backdrop

--- resources/views/company/branches/pics.blade.php
+++ resources/views/company/branches/pics.blade.php
@@ -263,7 +263,7 @@
         width: 100%;
         height: 100%;
         background: rgba(0, 0, 0, 0.5);
-        z-index: 1000;
+        z-index: 1055;
         align-items: center;
         justify-content: center;
     }
